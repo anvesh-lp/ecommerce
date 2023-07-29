cec2b942b7cbaded8ebc120b607f8961c137e148
anvesh-lp <knanvesh.01@gmail.com>
2023-07-29 14:32:01 -0500

    Creating directories/structure for the app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,7 +12,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ecommerce'`, () => {
+  it(`should h ave as title 'ecommerce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ecommerce');
