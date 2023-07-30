bd88676a387bdf28778aedf1ae22fd623bfa8d92
anvesh-lp <knanvesh.01@gmail.com>
2023-07-30 09:02:33 -0500

    Adding display recipe functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,10 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe("Biryani", "A spicy south Indian dish", "https://norecipes.com/wp-content/uploads/2017/05/chicken-biryani-006.jpg")
+    new Recipe("Biryani", "A spicy south Indian dish", "https://norecipes.com/wp-content/uploads/2017/05/chicken-biryani-006.jpg"),
+    new Recipe("Chicken Biryani", "A spicy south Indian dish", "https://norecipes.com/wp-content/uploads/2017/05/chicken-biryani-006.jpg")
+
   ];
+
 
 }
