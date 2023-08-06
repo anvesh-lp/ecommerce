36a47277c1ecbc8f1800709ef0a78af7bfe946fe
anvesh-lp <knanvesh.01@gmail.com>
2023-08-06 07:35:51 -0500

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,24 +1,31 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
-  selectedRecipeEmit = new EventEmitter<Recipe>();
+    selectedRecipeEmit = new EventEmitter<Recipe>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Biryani',
-      'A spicy south Indian dish',
-      'https://norecipes.com/wp-content/uploads/2017/05/chicken-biryani-006.jpg'
-    ),
-    new Recipe(
-      'Chicken Biryani',
-      'A spicy south Indian dish',
-      'https://recipe30.com/wp-content/uploads/2023/03/chicken-Biryani.jpg'
-    ),
-  ];
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Biryani',
+            'A spicy south Indian dish',
+            'https://norecipes.com/wp-content/uploads/2017/05/chicken-biryani-006.jpg',
+            [new Ingredient("rice", 2),
+                new Ingredient("water", 2),
+                new Ingredient("ghee", 3)]
+        ),
+        new Recipe(
+            'Chicken Biryani',
+            'A spicy south Indian dish',
+            'https://recipe30.com/wp-content/uploads/2023/03/chicken-Biryani.jpg',
+            [new Ingredient("rice", 2),
+                new Ingredient("water", 2),
+                new Ingredient("chicken", 3)]
+        ),
+    ];
 
 
-  getRecipes() {
-    return [...this.recipes];
-  }
+    getRecipes() {
+        return [...this.recipes];
+    }
 }
