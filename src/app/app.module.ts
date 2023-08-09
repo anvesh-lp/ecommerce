fcff32496cbd0010fc233854a950b00f05eba597
anvesh-lp <knanvesh.01@gmail.com>
2023-08-09 15:10:15 -0500

    adding routing functionality to the recipes and shopping list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import {AppComponent} from './app.component';
-import {RecipesComponent} from './recipes/recipes.component';
-import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
-import {HeaderComponent} from './header/header.component';
-import {NgOptimizedImage} from "@angular/common";
-import {DropdownDirective} from './shared/dropdown.directive';
-import {ShoppingListService} from "./shopping-list/shoppingList.service";
+import { AppComponent } from "./app.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
+import { HeaderComponent } from "./header/header.component";
+import { NgOptimizedImage } from "@angular/common";
+import { DropdownDirective } from "./shared/dropdown.directive";
+import { ShoppingListService } from "./shopping-list/shoppingList.service";
+import { appRouter } from "./appRouter.module";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        ShoppingListComponent,
-        ShoppingEditComponent,
-        HeaderComponent,
-      DropdownDirective,
-    ],
-    imports: [BrowserModule, NgOptimizedImage],
+  declarations: [
+    AppComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    HeaderComponent,
+    DropdownDirective,
+  ],
+  imports: [BrowserModule, NgOptimizedImage, appRouter],
   providers: [ShoppingListService],
-    bootstrap: [AppComponent],
+  bootstrap: [AppComponent,
 })
-export class AppModule {
-}
+export class AppModule {}
