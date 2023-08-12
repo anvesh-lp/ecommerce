7e2a469ce98b741873593a9294e467b938de4910
anvesh-lp <knanvesh.01@gmail.com>
2023-08-12 07:54:59 -0500

    adding routing functionality to the recipes and shopping list component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,7 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: 'header.component.html',
   selector: 'app-header',
 })
-export class HeaderComponent {
-
-  @Output() currentWindowClicked = new EventEmitter<String>();
-
-  // currentWindow:string="recipe"
-
-  onRecipeClicked() {
-    this.currentWindowClicked.emit("recipe");
-  }
-
-  onShoppingClicked() {
-    this.currentWindowClicked.emit("shopping");
-  }
-
-
-}
+export class HeaderComponent {}
